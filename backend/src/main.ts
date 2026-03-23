8bf6081de2b1377b8d66c226c84ca520c1b2ab68
shubhamvaishnav18 <shubhamvaishnav332@gmail.com>
2026-03-23 14:43:17 +0530

    changes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cors());
+  
+  app.enableCors(); 
+  
   app.setGlobalPrefix('api');
-  await app.listen(process.env.PORT ?? 3000);
+  
+  const port = process.env.PORT ?? 3000;
+  
+  await app.listen(port, '0.0.0.0');
+  
+  console.log(`Application is running on port: ${port}`);
 }
 bootstrap();
